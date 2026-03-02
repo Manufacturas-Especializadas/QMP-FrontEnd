91b136e611b7c366c9460a059ed61fb5a79985ea
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-02 10:44:07 -0600

    feat: metodo para obtener los procesos por linea

--- src/api/services/CatalogsService.ts
+++ src/api/services/CatalogsService.ts
@@ -1,10 +1,12 @@
 import { API_CONFIG } from "../../config/api";
-import type { Lines, Shifts } from "../../types/types";
+import type { Lines, Process, Shifts } from "../../types/types";
 import { apiClient } from "../client";
 
 class CatalogsService {
   private getLinesEndpoint = API_CONFIG.endpoints.catalags.getLines;
   private getShiftsEndpoint = API_CONFIG.endpoints.catalags.getShifts;
+  private getProcessByLineEndpoint =
+    API_CONFIG.endpoints.catalags.getProcessByLine;
 
   async getLines(): Promise<Lines[]> {
     return apiClient.get<Lines[]>(this.getLinesEndpoint);
@@ -12,6 +14,10 @@ class CatalogsService {
 
   async getShifts(): Promise<Shifts[]> {
     return apiClient.get<Shifts[]>(this.getShiftsEndpoint);
+  }
+
+  async getProcessByLine(id: number): Promise<Process[]> {
+    return apiClient.get<Process[]>(`${this.getProcessByLineEndpoint}${id}`);
   }
 }
 
