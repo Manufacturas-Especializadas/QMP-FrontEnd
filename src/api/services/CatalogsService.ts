ace2cf2875f44b245e421c17461bf6aaccd9b2cb
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-02 10:12:51 -0600

    feat: metodo para obtener los turnos

--- src/api/services/CatalogsService.ts
+++ src/api/services/CatalogsService.ts
@@ -1,12 +1,17 @@
 import { API_CONFIG } from "../../config/api";
-import type { Lines } from "../../types/types";
+import type { Lines, Shifts } from "../../types/types";
 import { apiClient } from "../client";
 
 class CatalogsService {
   private getLinesEndpoint = API_CONFIG.endpoints.catalags.getLines;
+  private getShiftsEndpoint = API_CONFIG.endpoints.catalags.getShifts;
 
   async getLines(): Promise<Lines[]> {
     return apiClient.get<Lines[]>(this.getLinesEndpoint);
+  }
+
+  async getShifts(): Promise<Shifts[]> {
+    return apiClient.get<Shifts[]>(this.getShiftsEndpoint);
   }
 }
 
