a3f49b9c32b6c029bf4200ac45ab23a7f95b9567
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-02 07:28:20 -0600

    feat: contenido principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
 import { Sidebar } from "./components/Sidebar/Sidebar";
+import { MyRoutes } from "./routes/Routes";
 
 export const App = () => {
   return (
-    <div>
-      <Sidebar />
-    </div>
+    <BrowserRouter>
+      <div className="flex min-h-screen bg-gray-50">
+        <Sidebar />
+        <main className="flex-1 flex flex-col min-w-0 overflow-hidden">
+          <MyRoutes />
+        </main>
+      </div>
+    </BrowserRouter>
   );
 };
