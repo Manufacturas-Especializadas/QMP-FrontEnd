462bc3737a6b1d7c430fc239a24e752d22530009
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-03 14:09:15 -0600

    feat: metodo para la verificacion

--- src/api/services/ScrapService.ts
+++ src/api/services/ScrapService.ts
@@ -1,10 +1,11 @@
 import { API_CONFIG } from "../../config/api";
-import type { Scrap, ScrapRead } from "../../types/types";
+import type { Scrap, ScrapRead, VerifyScrapPayload } from "../../types/types";
 import { apiClient } from "../client";
 
 class ScrapService {
   private getScrapByIdEndpoint = API_CONFIG.endpoints.scrap.getById;
   private createScrapEndpoint = API_CONFIG.endpoints.scrap.createScrap;
+  private verifyScrapEndpoint = API_CONFIG.endpoints.scrap.verify;
 
   async getScrapById(id: number): Promise<ScrapRead> {
     return apiClient.get<ScrapRead>(`${this.getScrapByIdEndpoint}${id}`);
@@ -12,6 +13,10 @@ class ScrapService {
 
   async createScrap(data: Scrap): Promise<void> {
     return apiClient.post<void>(this.createScrapEndpoint, data);
+  }
+
+  async verifyScrap(data: VerifyScrapPayload): Promise<void> {
+    return apiClient.patch<void>(this.verifyScrapEndpoint, data);
   }
 }
 
