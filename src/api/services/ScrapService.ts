f67c4bcdc9193a065e8e8f618a165eebb5a8d868
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-03 11:27:26 -0600

    feat: metodo para obtener por Id

--- src/api/services/ScrapService.ts
+++ src/api/services/ScrapService.ts
@@ -1,9 +1,14 @@
 import { API_CONFIG } from "../../config/api";
-import type { Scrap } from "../../types/types";
+import type { Scrap, ScrapRead } from "../../types/types";
 import { apiClient } from "../client";
 
 class ScrapService {
+  private getScrapByIdEndpoint = API_CONFIG.endpoints.scrap.getById;
   private createScrapEndpoint = API_CONFIG.endpoints.scrap.createScrap;
+
+  async getScrapById(id: number): Promise<ScrapRead> {
+    return apiClient.get<ScrapRead>(`${this.getScrapByIdEndpoint}${id}`);
+  }
 
   async createScrap(data: Scrap): Promise<void> {
     return apiClient.post<void>(this.createScrapEndpoint, data);
