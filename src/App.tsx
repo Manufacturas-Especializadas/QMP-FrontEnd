48d2e5bbaabb1f3ec33bc3ade441cc192898b04a
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-03 14:21:08 -0600

    fix: correcion de bug en sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -40,10 +40,14 @@ export const App = () => {
           },
         }}
       />
-      <div className="flex min-h-screen bg-gray-50">
+      <div className="flex h-screen bg-gray-50 overflow-hidden">
         <Sidebar />
-        <main className="flex-1 flex flex-col min-w-0 overflow-hidden">
-          <MyRoutes />
+
+        <main className="flex-1 flex flex-col min-w-0 overflow-y-auto">
+          <div className="p-4 md:p-0">
+            {" "}
+            <MyRoutes />
+          </div>
         </main>
       </div>
     </BrowserRouter>
