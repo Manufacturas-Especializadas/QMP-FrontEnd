6742b31064477e55f018411c0f5ed95bce3a6d07
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-06 09:47:26 -0600

    feat: metodo para actulizar una linea

--- src/api/services/LinesService.ts
+++ src/api/services/LinesService.ts
@@ -5,6 +5,7 @@ import { apiClient } from "../client";
 class LinesService {
   private getLineByIdEndpoint = API_CONFIG.endpoints.lines.lineById;
   private createEndpoint = API_CONFIG.endpoints.lines.create;
+  private updateEndpoint = API_CONFIG.endpoints.lines.update;
 
   async getLineById(id: number): Promise<LineRead> {
     return apiClient.get<LineRead>(`${this.getLineByIdEndpoint}${id}`);
@@ -12,6 +13,10 @@ class LinesService {
 
   async create(formData: LinesCreate): Promise<void> {
     await apiClient.post<void>(this.createEndpoint, formData);
+  }
+
+  async update(formData: LinesCreate, id: number): Promise<void> {
+    await apiClient.put<void>(`${this.updateEndpoint}${id}`, formData);
   }
 }
 
