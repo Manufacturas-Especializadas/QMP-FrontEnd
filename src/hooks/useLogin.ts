eb3a5c8b43fcb3fd59deb2fecd23bef660d01018
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-09 09:25:18 -0600

    feat: hook login

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { usersService } from "../api/services/UsersService";
+
+export const useLogin = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const loginUser = async (employeeNumber: string) => {
+    if (!employeeNumber.trim()) {
+      toast.error("Por favor, ingresa tu número de nómina");
+
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const response: any = await usersService.login({
+        employeeNumber,
+        password: employeeNumber,
+      });
+      console.log("Datos:", response);
+
+      localStorage.setItem("token", response.token);
+      localStorage.setItem("user", response.username);
+
+      toast.success(`Bienvenido, ${response.username}`);
+
+      navigate("/");
+    } catch (err: any) {
+      const msg = err.response?.data || "Credenciales incorrectas";
+      toast.error(msg);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loginUser, loading };
+};
