574ea4171db7ff7557edeae0257eda4fe09fb1be
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-09 10:38:17 -0600

    fix: se quito el sidebar y se agrego authprovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,52 @@
 import { BrowserRouter } from "react-router-dom";
-import { Sidebar } from "./components/Sidebar/Sidebar";
 import { MyRoutes } from "./routes/Routes";
 import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./context/AuthContext";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Toaster
-        toastOptions={{
-          className: "",
-          style: {
-            background: "#363636",
-            color: "#fff",
-            zIndex: 9999,
-          },
-          success: {
-            duration: 10000,
-            position: "top-right",
+    <AuthProvider>
+      <BrowserRouter>
+        <Toaster
+          toastOptions={{
+            className: "",
             style: {
-              background: "#10B981",
+              background: "#363636",
               color: "#fff",
+              zIndex: 9999,
             },
-          },
-          error: {
-            duration: 5000,
-            position: "top-right",
-            style: {
-              background: "#EF4444",
-              color: "#fff",
+            success: {
+              duration: 10000,
+              position: "top-right",
+              style: {
+                background: "#10B981",
+                color: "#fff",
+              },
             },
-          },
-          loading: {
-            duration: Infinity,
-            position: "top-right",
-            style: {
-              background: "#3B82F6",
-              color: "#fff",
+            error: {
+              duration: 5000,
+              position: "top-right",
+              style: {
+                background: "#EF4444",
+                color: "#fff",
+              },
             },
-          },
-        }}
-      />
-      <div className="flex min-h-screen bg-gray-50">
-        <Sidebar />
-        <main className="flex-1 flex flex-col min-w-0 overflow-hidden">
-          <MyRoutes />
-        </main>
-      </div>
-    </BrowserRouter>
+            loading: {
+              duration: Infinity,
+              position: "top-right",
+              style: {
+                background: "#3B82F6",
+                color: "#fff",
+              },
+            },
+          }}
+        />
+        <div className="flex min-h-screen bg-gray-50">
+          <main className="flex-1 flex flex-col min-w-0 overflow-hidden">
+            <MyRoutes />
+          </main>
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
   );
 };
