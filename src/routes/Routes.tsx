905f4d8dffe9e93e7324dd61182f632e68e4e844
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-09 09:26:10 -0600

    feat: ruta de registro y login

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -5,10 +5,15 @@ import { ScrapForm } from "../pages/Scrap/ScrapForm";
 import { Configuration } from "../pages/Configuration/Configuration";
 import { LinesConfig } from "../pages/Configuration/Lines/LinesConfig";
 import { ClientConfig } from "../pages/Configuration/Clients/ClientConfig";
+import { Register } from "../pages/Auth/Register";
+import { Login } from "../pages/Auth/Login";
 
 export const MyRoutes = () => {
   return (
     <Routes>
+      <Route path="/register" element={<Register />} />
+
+      <Route path="/login" element={<Login />} />
       <Route path="/" element={<ProcessAudit />} />
       <Route path="/scrap" element={<ScrapIndex />} />
       <Route path="/scrap/registro" element={<ScrapForm />} />
