badb130a4d996de826e91b80f3b92b0b617eead4
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-24 11:31:33 -0600

    feat: metodo para cambiar el estatus del usuario

--- src/api/services/UsersService.ts
+++ src/api/services/UsersService.ts
@@ -6,6 +6,7 @@ class UsersService {
   private usersListEndpoint = API_CONFIG.endpoints.user.users;
   private registerEndpoint = API_CONFIG.endpoints.user.register;
   private loginEndpoint = API_CONFIG.endpoints.user.login;
+  private toggleStatusEndpoint = API_CONFIG.endpoints.user.toogleStatus;
 
   async usersList(): Promise<UsersList[]> {
     return apiClient.get<UsersList[]>(this.usersListEndpoint);
@@ -17,6 +18,12 @@ class UsersService {
 
   async login(data: UserLogin): Promise<void> {
     return apiClient.post<void>(this.loginEndpoint, data);
+  }
+
+  async toggleStatus(username: string): Promise<void> {
+    const url = `${this.toggleStatusEndpoint}${username}`;
+
+    return apiClient.patch<void>(url);
   }
 }
 
