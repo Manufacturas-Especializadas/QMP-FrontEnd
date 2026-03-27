f92bb360cb4f09be875d88df9e3030fc992d0ce4
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-27 08:02:05 -0600

    feat: metodo para descargar el reporte

--- src/api/services/ScrapService.ts
+++ src/api/services/ScrapService.ts
@@ -4,11 +4,34 @@ import { apiClient } from "../client";
 
 class ScrapService {
   private getScrapByIdEndpoint = API_CONFIG.endpoints.scrap.getById;
+  private getReportsEndpoint = API_CONFIG.endpoints.scrap.reports;
   private createScrapEndpoint = API_CONFIG.endpoints.scrap.createScrap;
   private verifyScrapEndpoint = API_CONFIG.endpoints.scrap.verify;
 
   async getScrapById(id: number): Promise<ScrapRead> {
     return apiClient.get<ScrapRead>(`${this.getScrapByIdEndpoint}${id}`);
+  }
+
+  async getReports(): Promise<void> {
+    const now = new Date();
+    const meses = [
+      "Enero",
+      "Febrero",
+      "Marzo",
+      "Abril",
+      "Mayo",
+      "Junio",
+      "Julio",
+      "Agosto",
+      "Septiembre",
+      "Octubre",
+      "Noviembre",
+      "Diciembre",
+    ];
+
+    const fileName = `Reporte_Scrap_${meses[now.getMonth()]}_${now.getFullYear()}.xlsx`;
+
+    return apiClient.downloadFile(this.getReportsEndpoint, fileName);
   }
 
   async createScrap(data: Scrap): Promise<void> {
