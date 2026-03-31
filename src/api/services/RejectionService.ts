2eb9f414105ddd5db1260a4ffbc32f1c459aed63
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-31 09:52:13 -0600

    feat: metodo para el registro de rechazos

--- src/api/services/RejectionService.ts
+++ src/api/services/RejectionService.ts
@@ -3,9 +3,14 @@ import { apiClient } from "../client";
 
 class RejectionService {
   private getNextFolioEndpoint = API_CONFIG.endpoints.rejections.getNextFolio;
+  private creatRejectionEndpoint = API_CONFIG.endpoints.rejections.create;
 
   async getNextFolio(): Promise<number> {
     return apiClient.get<number>(this.getNextFolioEndpoint);
+  }
+
+  async createRejection(data: FormData): Promise<void> {
+    return apiClient.post<void>(this.creatRejectionEndpoint, data);
   }
 }
 
