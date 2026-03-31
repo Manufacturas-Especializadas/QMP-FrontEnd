d35df728e893b0eb06f27dd9e3281f7bc49d549a
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-31 14:42:37 -0600

    feat: metodo para actulizar un registro

--- src/api/services/RejectionService.ts
+++ src/api/services/RejectionService.ts
@@ -4,6 +4,7 @@ import { apiClient } from "../client";
 class RejectionService {
   private getNextFolioEndpoint = API_CONFIG.endpoints.rejections.getNextFolio;
   private creatRejectionEndpoint = API_CONFIG.endpoints.rejections.create;
+  private updateRejectionEndpoint = API_CONFIG.endpoints.rejections.update;
 
   async getNextFolio(): Promise<number> {
     return apiClient.get<number>(this.getNextFolioEndpoint);
@@ -11,6 +12,10 @@ class RejectionService {
 
   async createRejection(data: FormData): Promise<void> {
     return apiClient.post<void>(this.creatRejectionEndpoint, data);
+  }
+
+  async updateRejection(id: number, data: FormData): Promise<void> {
+    return apiClient.put<void>(`${this.updateRejectionEndpoint}${id}`, data);
   }
 }
 
