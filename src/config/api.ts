12009ef8a20d0154d0c2ad92d79484690c068b44
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-31 14:41:00 -0600

    feat: endpoint para la actualizar los rechazos

--- src/config/api.ts
+++ src/config/api.ts
@@ -31,6 +31,7 @@ export const API_CONFIG = {
     rejections: {
       getNextFolio: "/api/Rejections/GeNextFolio",
       create: "/api/Rejections/Create",
+      update: "/api/Rejections/Edit/",
     },
     scrap: {
       getById: "/api/Scrap/ById/",
