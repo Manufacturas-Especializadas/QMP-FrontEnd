5ced76a18b2351a268dba95689a205570f8fb2bd
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-03 14:21:16 -0600

    feat(service): metodo para actualizar el usuario

--- src/api/services/UsersService.ts
+++ src/api/services/UsersService.ts
@@ -1,15 +1,25 @@
 import { API_CONFIG } from "../../config/api";
-import type { UserLogin, UserRegister, UsersList } from "../../types/types";
+import type {
+  EditUserPayload,
+  UserLogin,
+  UserRegister,
+  UsersList,
+} from "../../types/types";
 import { apiClient } from "../client";
 
 class UsersService {
   private usersListEndpoint = API_CONFIG.endpoints.user.users;
   private registerEndpoint = API_CONFIG.endpoints.user.register;
+  private updateEndpoint = API_CONFIG.endpoints.user.update;
   private loginEndpoint = API_CONFIG.endpoints.user.login;
   private toggleStatusEndpoint = API_CONFIG.endpoints.user.toogleStatus;
 
   async usersList(): Promise<UsersList[]> {
     return apiClient.get<UsersList[]>(this.usersListEndpoint);
+  }
+
+  async update(data: EditUserPayload): Promise<void> {
+    return apiClient.put<void>(this.updateEndpoint, data);
   }
 
   async register(data: UserRegister): Promise<void> {
