bf28631fca827329100e112f8826bcad757e18c8
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-09 12:13:15 -0600

    style(components): propiedades responsives

--- src/components/Layouts/MainLayout/MainLayout.tsx
+++ src/components/Layouts/MainLayout/MainLayout.tsx
@@ -5,7 +5,7 @@ export const MainLayout = () => {
   return (
     <div className="flex h-screen bg-slate-50 overflow-hidden">
       <Sidebar />
-      <main className="flex-1 overflow-y-auto p-4 md:p-8">
+      <main className="flex-1 overflow-y-auto pt-20 px-4 pb-4 md:p-8">
         <Outlet />
       </main>
     </div>
