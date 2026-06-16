b46a859486d51d78e185dc024063986234c28ead
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-16 15:09:30 -0600

    feat(service): metod para el registro de auditorias FCDS

--- src/api/services/AuditsFcdsService.ts
+++ src/api/services/AuditsFcdsService.ts
@@ -1,3 +1,13 @@
-class AuditsFcdsService {}
+import { API_CONFIG } from "../../config/api";
+import type { CreateAuditFcds } from "../../types/types";
+import { apiClient } from "../client";
+
+class AuditsFcdsService {
+  private createEndpoint = API_CONFIG.endpoints.auditsFCDS.create;
+
+  async create(data: CreateAuditFcds): Promise<{ message: string }> {
+    return apiClient.post<{ message: string }>(this.createEndpoint, data);
+  }
+}
 
 export const auditsFcdsService = new AuditsFcdsService();
