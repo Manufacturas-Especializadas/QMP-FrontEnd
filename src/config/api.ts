44b407ef43bb072edda40a4f095190b7b1c80c4c
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-17 08:50:30 -0600

    feat(api): url del endpoint para obtener las categorias del operador

--- src/config/api.ts
+++ src/config/api.ts
@@ -26,6 +26,7 @@ export const API_CONFIG = {
       getScrap: "/api/Catalogs/Scrap",
       getRejections: "/api/Catalogs/Rejections",
       getConditionByDefect: "/api/Catalogs/conditions/",
+      getCategoryOperators: "/api/Catalogs/categorys",
       getProcessByLine: "/api/Catalogs/process/",
       getMachineCodesByProcess: "/api/Catalogs/machineCodes/",
       getMachinesByLines: "/api/Catalogs/machinesByLines",
