1994bb3509eefd198c824bf5b6d3900a660f5f14
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-18 09:28:27 -0600

    feat(service): metodo para obtener la lista de auditorias

--- src/api/services/AuditsFcdsService.ts
+++ src/api/services/AuditsFcdsService.ts
@@ -1,9 +1,14 @@
 import { API_CONFIG } from "../../config/api";
-import type { CreateAuditFcds } from "../../types/types";
+import type { AuditFcdsList, CreateAuditFcds } from "../../types/types";
 import { apiClient } from "../client";
 
 class AuditsFcdsService {
+  private getAuditsFcdsEndpoint = API_CONFIG.endpoints.auditsFCDS.getAuditFcds;
   private createEndpoint = API_CONFIG.endpoints.auditsFCDS.create;
+
+  async getAuditsFcds(): Promise<AuditFcdsList[]> {
+    return apiClient.get<AuditFcdsList[]>(this.getAuditsFcdsEndpoint);
+  }
 
   async create(data: CreateAuditFcds): Promise<{ message: string }> {
     return apiClient.post<{ message: string }>(this.createEndpoint, data);
