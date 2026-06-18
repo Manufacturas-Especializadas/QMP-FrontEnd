f38c0b29a73a2d32463b5ef8906650afcdf1e8db
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-18 10:27:46 -0600

    feat(service): metodo para obtener los detalles por id

--- src/api/services/AuditsFcdsService.ts
+++ src/api/services/AuditsFcdsService.ts
@@ -1,13 +1,22 @@
 import { API_CONFIG } from "../../config/api";
-import type { AuditFcdsList, CreateAuditFcds } from "../../types/types";
+import type {
+  AuditFcdsList,
+  CreateAuditFcds,
+  DetailedAuditFcds,
+} from "../../types/types";
 import { apiClient } from "../client";
 
 class AuditsFcdsService {
   private getAuditsFcdsEndpoint = API_CONFIG.endpoints.auditsFCDS.getAuditFcds;
+  private getByIdEndpoint = API_CONFIG.endpoints.auditsFCDS.getById;
   private createEndpoint = API_CONFIG.endpoints.auditsFCDS.create;
 
   async getAuditsFcds(): Promise<AuditFcdsList[]> {
     return apiClient.get<AuditFcdsList[]>(this.getAuditsFcdsEndpoint);
+  }
+
+  async getById(id: number): Promise<DetailedAuditFcds> {
+    return apiClient.get<DetailedAuditFcds>(`${this.getByIdEndpoint}${id}`);
   }
 
   async create(data: CreateAuditFcds): Promise<{ message: string }> {
