1a4024da0deca9093a4ab3a876761a2badb28bb1
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-23 14:44:18 -0600

    feat(type): type audits points

--- src/types/types.ts
+++ src/types/types.ts
@@ -125,6 +125,11 @@ export interface WallsOfDiameters {
   name: string;
 }
 
+export interface AuditsPoints {
+  id: number;
+  name: string;
+}
+
 export interface UsersList {
   id: number;
   payRollNumber: string;
