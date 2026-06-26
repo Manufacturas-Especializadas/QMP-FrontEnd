f80981ee095ce10ee1434b1d836c5d528ff86607
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-26 14:20:03 -0600

    feat(route): ruta hacia el componente de configuracion de codigos de maquinas

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -19,6 +19,7 @@ import { AuditsScrap } from "../pages/AuditsScrap/AuditsScrap";
 import { AuditsScrapReport } from "../pages/AuditsScrap/AuditsScrapReport";
 import { AuditsACDS } from "../pages/AuditsACDS/AuditsACDS";
 import { ProcesesConfig } from "../pages/Configuration/Processes/ProcesesConfig";
+import { MachineCodesConfig } from "../pages/Configuration/MachineCodes/MachineCodesConfig";
 
 export const MyRoutes = () => {
   return (
@@ -50,6 +51,10 @@ export const MyRoutes = () => {
             <Route path="/config" element={<Configuration />} />
             <Route path="/config/lineas" element={<LinesConfig />} />
             <Route path="/config/procesos" element={<ProcesesConfig />} />
+            <Route
+              path="/config/codigo-maquinas"
+              element={<MachineCodesConfig />}
+            />
             <Route path="/config/clientes" element={<ClientConfig />} />
             <Route path="/config/scrap" element={<ScrapConfig />} />
           </Route>
