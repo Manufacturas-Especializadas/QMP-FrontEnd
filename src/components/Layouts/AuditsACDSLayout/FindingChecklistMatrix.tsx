99ccaa79ce74e21ab79783ecca0456ca54521888
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-03 08:07:56 -0600

    feat(component): nuevas preguntas

--- src/components/Layouts/AuditsACDSLayout/FindingChecklistMatrix.tsx
+++ src/components/Layouts/AuditsACDSLayout/FindingChecklistMatrix.tsx
@@ -58,10 +58,13 @@ export const FindingChecklistMatrix = ({
           { label: "Vista Lateral", key: "sideView" },
           { label: "Vista Superior", key: "topView" },
           { label: "Vista Isométrica", key: "isometricView" },
+          { label: "PP BOM", key: "ppBom" },
+          { label: "Defectos de Soldadura", key: "weldingDefects" },
         ].map((v) => (
           <div
             key={v.key}
-            className="flex justify-between items-center py-1.5 border-b border-slate-50 last:border-0"
+            className="flex justify-between items-center py-1.5 border-b border-slate-50 
+            last:border-0"
           >
             <span className="text-xs font-bold text-slate-600 uppercase">
               {v.label}
@@ -71,18 +74,26 @@ export const FindingChecklistMatrix = ({
                 { v: 1, l: "Cumple" },
                 { v: 2, l: "No Cumple" },
                 { v: 3, l: "N/A" },
-              ].map((o) => (
-                <button
-                  key={o.v}
-                  type="button"
-                  onClick={() =>
-                    setCurrentFinding((p: any) => ({ ...p, [v.key]: o.v }))
-                  }
-                  className={`px-2.5 py-1 rounded text-[8px] uppercase font-black transition-all cursor-pointer ${currentFinding[v.key] === o.v ? "bg-slate-800 text-white shadow-sm" : "text-slate-400 hover:text-slate-600"}`}
-                >
-                  {o.l}
-                </button>
-              ))}
+              ].map((o) => {
+                const isSelected = currentFinding[v.key] === o.v;
+                return (
+                  <button
+                    key={o.v}
+                    type="button"
+                    onClick={() =>
+                      setCurrentFinding((p: any) => ({ ...p, [v.key]: o.v }))
+                    }
+                    className={`px-2.5 py-1 rounded text-[8px] uppercase font-black 
+                        transition-all cursor-pointer ${
+                          isSelected
+                            ? "bg-slate-800 text-white shadow-sm"
+                            : "text-slate-400 hover:text-slate-600"
+                        }`}
+                  >
+                    {o.l}
+                  </button>
+                );
+              })}
             </div>
           </div>
         ))}
